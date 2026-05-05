0b8eb96844b2468f75118e36e5782fa2ef4bbfaa
tsia-M <tsiamosala56@gmail.com>
2026-05-05 23:30:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const weekDayArray = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+const monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'Augugst', 'September', 'October', 'November', 'December'];
+
+const calendarMonthElem = document.querySelector('.calendar-month');
+const calendarDayElem = document.querySelector('.calendar-day');
+const calendarDateElem = document.querySelector('.calendar-date');
+const calendarYearElem = document.querySelector('.calendar-year');
+
+const dateObject = new Date();
+
+const year = dateObject.getFullYear();
+const month = dateObject.getMonth();
+const day = dateObject.getDay();
+const date = dateObject.getDate();
+
+calendarMonthElem.innerText = `${monthArray[month]}`;
+calendarDayElem.innerText = `${weekDayArray[day]}`;
+calendarDateElem.innerText = `${date}`;
+calendarYearElem.innerText = `${year}`;
